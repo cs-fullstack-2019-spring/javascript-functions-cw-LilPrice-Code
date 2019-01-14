5d39feb47258f0eb8a894e139d1a2c728e8f8346
LilPrice-Code <littleprice002@gmail.com>
2019-01-13 18:37:21 -0600

    Graded Classowrk

---
+++ Pro1.js
@@ -0,0 +1,9 @@
+function printNumbers(num1, num2)
+{
+    for (var i = num1;i <= num2; i++)
+    {
+        console.log(i)
+    }
+}
+
+var numbers = printNumbers(-25, 20);
---
+++ Pro2.js
@@ -0,0 +1,5 @@
+function name(firstname, lastname)
+{
+    console.log(lastname, firstname);
+}
+var fullName =name("Shoshard", "Price");
